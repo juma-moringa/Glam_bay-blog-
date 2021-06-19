a393cb8ac1ffb7072a773185bf3c6b1d0b3414bd
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-19 18:24:01 +0300

    updated the blogs form for the users updates for the previous blogs

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,25 +4,25 @@ from wtforms import (StringField, TextAreaField,
 from wtforms.validators import Required
 
 
-
+# user profile form
 class UserProfile(FlaskForm):
     first_name = StringField("First name...")
     email = StringField(" Your Email")
     last_name = StringField("Last Name...")
     bio = TextAreaField("Bio")
     submit = SubmitField("Update")
-
+# blog form
 class BlogForm(FlaskForm):
     title = StringField("Blog title:", validators=[Required()])
     post = TextAreaField("Type Away:", validators=[Required()])
     submit = SubmitField("Post")
-
+#comments form
 class CommentsForm(FlaskForm):
     comment = TextAreaField("Post Comment", validators=[Required()])
     name = StringField("Comment Name")
     submit = SubmitField("Comment")
 
-
+#update blogs form
 class UpdateBlogForm(FlaskForm):
     title = StringField("Blog title", validators=[Required()])
     blog = TextAreaField("Your blog", validators=[Required()])
