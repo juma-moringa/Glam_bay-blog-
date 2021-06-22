112d52a97da3d8dc9ec0b2a88a0c62c09e55e337
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-22 13:13:17 +0300

    added mail functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,7 +30,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        # mail_message("Welcome to Glam_bay blogs","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Glam_bay blogs","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/register.html',registration_form = form)
